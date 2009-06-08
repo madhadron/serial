4c8fac3580de570f264d3510bc4fe8132e125a0d
Frederick Ross <madhadron@gmail.com>
2009-06-08 11:13:12 +0200

    Resolved -Wall warnings I had missed.

--- System/Serial/BlockingManager.hs
+++ System/Serial/BlockingManager.hs
@@ -6,7 +6,7 @@ import Control.Concurrent
 import Control.Concurrent.MVar
 import Control.Monad
 
-type BlockingSerialCommand = (String, MVar String)
+type BlockingSerialCommand = (String, MVar (Maybe String))
 type BlockingSerialManager = MVar BlockingSerialCommand
 
 -- | The blocking 'serialManager' function takes one additional argument, the timeout (since it cannot continue executing commands in parallel while one command freezes).
@@ -17,24 +17,30 @@ serialManager h timeout = do mv <- newEmptyMVar
                              forkIO $ process h mv timeout
                              return mv
 
+process :: Handle
+        -> MVar (String, MVar (Maybe [Char]))
+        -> Int
+        -> IO ()
 process h mv timeout = do (cmd,resVar) <- takeMVar mv
                           hPutStr h cmd
                           r <- hWaitForInput h timeout
-                          let loop = do st <- hReady h
-                                        if st then do l <- hGetLine h
-                                                      q <- loop
-                                                      return (l ++ q)
-                                          else return "" 
-                          res <- loop
-                          putMVar resVar res
-                          process h mv timeout
+                          if not r then putMVar resVar Nothing 
+                           else do
+                              let loop = do st <- hReady h
+                                            if st then do l <- hGetLine h
+                                                          q <- loop
+                                                          return (l ++ q)
+                                             else return "" 
+                              res <- loop
+                              putMVar resVar (Just res)
+                              process h mv timeout
 
 -- | Wrapping commands is identical to the non-blocking version except that there is no predicate to recognize return values.
 wrapCommand :: String -- ^ The end of line character for this port
             -> String -- ^ The command to send
             -> BlockingSerialManager -- ^ The serial port to access
-            -> IO String
+            -> IO (Maybe String) -- ^ 'Nothing' if there was a timeout, other 'Just' and the response string
 wrapCommand eol cmd mgr = do
   mv <- newEmptyMVar
-  putMVar mgr (cmd, mv)
+  putMVar mgr (cmd ++ eol, mv)
   takeMVar mv
